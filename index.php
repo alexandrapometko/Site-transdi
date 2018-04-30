1bec4fa08b310007dd12d7179c8058918fa1b5a7
AlexandraP <apometko@ensc.fr>
2018-04-30 11:15:01 +0200

    removed all the <br>, includes
    Cause apprently it's nasty to code with them

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<?php include('includes/head.html'); ?>
+
+<body>
+	<?php include('includes/top.html'); ?>
+	
+	<!--team-->
+	<div class="team container text-center">
+		<div class="row">
+			<div class="col-md-4">
+				<img src="images/team/anthony.jpg" alt="Anthony"></img>
+				<p><strong>Anthony</strong></p>
+				L'exigeant
+			</div>
+			<div class="col-md-4">
+				<img src="images/team/julien.jpg" alt="Julien"></img>
+				<p><strong>Julien</strong></p>
+				Le curieux
+			</div>
+			<div class="col-md-4">
+				<img src="images/team/elise.jpg" alt="Elise"></img>
+				<p><strong>Elise</strong></p>
+				La force tranquille
+			</div>
+		</div><br>
+		<div class="row">
+			<div class="col-md-5">
+				<img src="images/team/claire.jpg" alt="Claire"></img>
+				<p><strong>Claire</strong></p>
+				L'endurante
+			</div>
+			<div class="col-md-2">
+				<h1>Equipe de choc</h1>
+				<p>Pour tous vos besoins en UX dans le domaine clinique.</p>
+			</div>
+			<div class="col-md-5">
+				<img src="images/team/sacha.jpg" alt="Sacha"></img>
+				<p><strong>Alexandra</strong> </p>
+				L'optimiste
+			</div>
+		</div>
+	</div><hr>
+	
+	<!--Presentation-->
+	<div class="container text-center">
+		<div class="present text">
+					<p>Au cours de cette première année à l'ENSC, nous avons eu l'opportunité de travailler sur <strong>deux projets</strong> différents.</p>
+					
+			<div class="row">
+				<div class="col-sm-1 col-md-1"></div>
+				<div class="col-sm-5 col-md-5 project p1">
+					<h1>THALIE</h1>
+					<h3>Premier semestre</h3>
+					<p>Au cours de ce premier semestre, nous évalué l'<strong>utilisabilité</strong> de ce logiciel de suivi de troubles cognitifs. </p>
+					<p>Tout ceci en faisant des retours constants à une équipe pluridisciplinaire de développeurs, de chercheurs et de neuropsychologues.</p>
+					<p>De plus, nous avons réalisé un manuel utilisateur <strong>visuel</strong>, <strong>rapide</strong> et <strong>ludique</strong> à destination des neuropsychologues qui vont utiliser THALIE.</p>
+					<p><a href="html/thalie.html" title="Informations sur THALIE">Cliquez ici pour en savoir plus !</a></p>
+				</div>
+				
+				<div class="col-sm-5 col-md-5 project p2">
+					<h1>Bizi&Co</h1>
+					<h3>Deuxième semestre</h3>
+					<p>"Guztiak Bizi" est une association qui lutte contre l'isolement des personnes âgées grâce à une plateforme d'échange de services entre bénévoles.</p>
+					<p>Pour utiliser cette plateforme, les personnes âgées passent par l'intermédiaire de la Google Home.<p>
+					<p>Notre but est d'évaluer l'<strong>acceptabilité</strong> de ce service. Mais ce dispositif pourrait aussi être utilisé dans l'optique d'effectuer un <strong>suivi cardiologique</strong> régulier.</p>
+					<p><a href="html/bizi.html" title="Informations sur Bizi">Cliquez ici pour en savoir plus !</a></p>
+				</div><br>
+				<div class="col-sm-1 col-md-1"></div>
+			</div>
+			<div class="row">
+				<a href="html/why2.html" title="Pourquoi deux projets ?"><p>Pourquoi avons nous réalisé deux projets ?</a>
+			</div>
+		</div>
+	</div>	
+	
+	<!--GESP-->
+	<div class="jumbotron gesp text-center"><a href="html/gesp.html" title="Gestion de projet"><h3>La <strong>gestion de projet</strong>, c'est par ici !</h3></a></div>
+	
+	<?php include('includes/footer.html'); ?>
+
+  </body>
+</html>
