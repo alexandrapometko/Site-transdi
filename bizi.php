aadb38484b8d43a70fa0aa40d15efc07742079a6
alexandrapometko <apometko@ensc.fr>
2018-05-01 23:49:05 +0200

    Bizi finished

--- bizi.php
+++ bizi.php
@@ -5,10 +5,71 @@
 <body>
 	<?php include('includes/top.html'); ?>
 	<div class="container text-center">
+		<img id="logobizi" src="images/bizi.png" alt="Bizi&Co"></img>	
 
-	<img src="images/bizi.png" alt="Bizi&Co"></img>
+		<p>L’objectif du projet Bizi porté par l’association Guztiak Bizi est de prévenir et <strong>lutter contre l’isolement social</strong> des seniors et des personnes en situation de handicap. 
+		<h4>Le principe</h4>
+		<img id="oscar" src="images/oscar.jpg" alt="Bizi&Co"/>
+		
+		<p>L’idée est de développer une plateforme d’échanges et d’entraide qui vise à améliorer la vie quotidienne de ces personnes en utilisant des technologies innovantes adaptées.</p>
+		<p>La plateforme doit permettre un partage d’informations individuelles et collectives afin de constituer un <strong>réseau d’échange de services</strong>. 
+		Elle sera ainsi capable de référencer toutes les ressources disponibles autour de la personne, offrant ainsi un panel de possibilités de services à une demande donnée.</p>
+		<p>Il y a également une volonté de contribuer à améliorer la santé des populations ciblées en
+		développant une démarche de e-santé et santé connectée pour le <strong>suivi des patients chroniques</strong> (patients diabétiques, cardio…).</p>
+		
+		<h4>Le support</h4>
+		<p>Cette plateforme s’utilise par le biais d'un smartphone, d'un navigateur ou plus intéressant plus les personnes âgées, d’un boitier à commande vocale : la Google Home.</p>
+		<img id="gh" src="images/gh.png" alt="Google Home Mini"/>
+		<h3>Notre action</h3>
+		
+		<div class="project">
+			<h4><strong>Nos objectifs</strong></h4>
+			<p>Notre objectif dans le cadre du projet transdisciplinaire était de réaliser une enquête en population sur l’intérêt, <strong>l'acceptabilité et l'utilisabilité</strong> 
+			de la plateforme par les potentiels utilisateurs (seniors).</p>
+			<p>Il s’agissait également d’évaluer une seconde plateforme développée par  Nicolas Pages, anesthésiste au CHU. Cette dernière est une application pour smartphone qui réalise 
+			le même suivi cardiologique qui est implenté dans Bizi sur la Google Home, mais en utilisant un support écrit. Ceci allait nous permettre de comparer les 2 interfaces et voir celle qui était le mieux acceptée.</p>
+		</div>	
+		<div class="project">
+			<h4><strong>Travail effectué</strong></h4>
+			<p>Pour mener cette enquête, nous avons dû mettre en place des protocoles de passation de test utilisateurs et de réalisation d’entretiens. </p>
+			<p>Il y a eu plusieurs changement de protocole au cours de projet en raison d’évolution des attentes du client. Ces derniers ont été détaillés dans la partie <a href="gesp.php" title="GESP">"Gestion de projet"</a>.</p>
 
-
+			<p>Les tests utilisateurs ont été réalisé au sein du service cardiologique de l’hôpital Haut-Lévêque de Pessac sur des patients entre 65 et 90 ans environ.</p>
+			<div class="inside">
+				<h4>Premier protocole</h4>
+				<p>Dans un premier temps, nous avons effectué des tests utilisateurs axés sur l'aspect suivi cardiologique.</p>
+				<p>Premièrement avec le boitier à commande vocale puis avec l’application mobile, avant et après chaque test nous avons fait passer des entretiens pour connaître le 
+				rapport aux technologies des personnes et recueillir leurs ressentis. </p>
+				<p>L’entretien se terminait  par des questions générales sur le concept Bizi&Co afin d’estimer de le
+				degré éventuel d’implication et d’utilisation du dispositif.</p>
+				
+				<strong>Déroulement du test utilisateur</strong>
+				<ol>
+					<li>Passation de l’entretien 1 : questions personnelles, opinion sur le dispositif  à commandes vocales</li>
+					<li>Utilisation de la Google Home : questionnaire de suivi cardiologique</li>
+					<li>Passation de l’entretien sur le dispositif </li>
+					<li>Entretien sur l’opinion sur l’utilisation d’un smartphone et sur l'application mobile (sans utilisation)</li>
+					<li>Utilisation de l’application mobile </li>
+					<li>Passation de l’entretien de comparaison des deux interfaces</li>
+					<li>Questions sur le concept de Bizi & Co</li>
+				</ol>
+			</div>
+			<div class="inside">
+				<h4>Deuxième protocole</h4>
+				<p>Par la suite, nous avons modifié le protocole de test en supprimant la partie application mobile, mais également en changeant l’approche du protocole suivi par la Google Home afin d’humaniser davantage et alleger nos tests. 
+				Ainsi, le suivi cardiologique a fait place à une présentation normale de l'utilisateur.</p>
+				<p>Les utilisateurs passaient dans un premier temps un test où ils réalisaient le questionnaire de présentation de l’application Bizi&Co (toujours avec la Google Home).</p>
+				<p>Cela permet à l’utilisateur d'assimiler ses informations et ses besoins au sein de la plateforme de l’application. La suite était identique au protocole initial.</p>
+			</div>
+		</div>
+		<div class="project">
+			<h4><strong>Difficultés rencontrées</strong></h4>
+			<p>Le cadre hospitalier n’était pas le plus propice à la réalisation de ces tests utilisateurs, les personnes y sont souvent stressées et fatiguées donc peu réceptives. </p>
+			<p>Nous sommes donc entrés en contact avec Karine PERES, aussi partenaire du projet Bizi&Co,  afin d’organiser des tests utilisateurs en EHPAD ou en clubs séniors. </p>
+			<p>Cependant, il manquait de dispositifs pour pouvoir utiliser le boitier à commande vocale (notamment du wifi), ces tests n’ont donc pas pu avoir lieu.</p>
+		
+		</div>
+		<img id="team" src="images/team/thegreat3.jpg" alt="Une équipe prête à tout !"/>
 	</div>
 	<?php include('includes/footer.html'); ?>
 
--- thalie.php
+++ thalie.php
@@ -5,6 +5,7 @@
 <body>
 	<?php include('includes/top.html'); ?>
 	
+
 	
 	<?php include('includes/footer.html'); ?>
 
