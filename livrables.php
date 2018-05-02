1af92dc61f292f5bb5acf16df4e5a3e2a273b42b
alexandrapometko <apometko@ensc.fr>
2018-05-02 11:00:00 +0200

    Added downloads

--- livrables.php
+++ livrables.php
@@ -17,14 +17,14 @@
 		<tr>
 			<td>Cahier des charges</td>
 			<td>Décrit les objectifs du premier semestre ainsi que les spécifications techniques du manuel utilisateur</td>
-			<td>50 Mo</td>
-			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
+			<td>0,6 Mo</td>
+			<td><a href="livrables/CDC.pdf" title="télécharger" target="_blank"><span class="glyphicon glyphicon-download-alt"></span></a></td>				
 		</tr>
 		<tr>
 			<td>Manuel utilisateur</td>
-			<td>Site web contenant les tutoriels à destination des neuropsychologues pour utiliser THALIE.</td>
-			<td>700 Mo</td>
-			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
+			<td>Site web contenant les tutoriels à destination des neuropsychologues pour utiliser THALIE. (lien vers l'archive Drive)</td>
+			<td>174 Mo</td>
+			<td><a href="https://drive.google.com/open?id=19kdW1xRUlc9vAQF2D3mRLDMHJwVh3ADa" title="lien vers le manuel" target="_blank"><span class="glyphicon glyphicon-download-alt"></span></a></td>				
 		</tr>
 		</table> <hr>
 		
@@ -36,32 +36,32 @@
 		</tr>
 		<tr>
 			<td>Protocole de tests initial</td>
-			<td></td>
-			<td>50 Mo</td>
-			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
+			<td>Protocole de test incluant le suivi cardiologique et l'application mobile</td>
+			<td>0,1 Mo</td>
+			<td><a href="livrables/protocole1_bizi.pdf" title="télécharger" target="_blank"><span class="glyphicon glyphicon-download-alt"></span></a></td>				
 		</tr>
 		<tr>
 			<td>Deuxième protocole de tests</td>
-			<td></td>
-			<td> Mo</td>
-			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
+			<td>Protocole de test axé uniquement sur le scénario de présentation et Bizi&Co</td>
+			<td>0,1 Mo</td>
+			<td><a href="livrables/protocole2_bizi.pdf" title="télécharger" target="_blank"><span class="glyphicon glyphicon-download-alt"></span></a></td>				
 		</tr>
 		<tr>
 			<td>Notice pour les questions</td>
-			<td></td>
-			<td> Mo</td>
-			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
+			<td>Aide pour poser des questions compréhensibles par les personnes âgées qui amènenent aux réponses les plus pertinente pour la Google Home.</td>
+			<td>0,2 Mo</td>
+			<td><a href="livrables/notice_questions.pdf" title="télécharger" target="_blank"><span class="glyphicon glyphicon-download-alt"></span></td>				
 		</tr>
 		<tr>
 			<td>Résultats de l'étude</td>
 			<td>Rapport final pour présenter les résultats des tests utilisateurs et nos conclusions sur l'utilisabilité du service</td>
-			<td> Mo</td>
+			<td>Mo</td>
 			<td><span class="glyphicon glyphicon-download-alt"></span></td>				
 		</tr>
 		</table>
 		<hr>
-		<h3>Télécharger une archive contenant tous les documents<h3>
-		<p><span class="glyphicon glyphicon-download-alt"></span></p>
+		<h3>Télécharger une archive contenant tous les documents<br><br><span class="glyphicon glyphicon-download-alt"></span></h3>
+		<p>Le manuel utilisateur n'est pas inclus du fait de son poids.</p>
 		
 		
 	</div>
