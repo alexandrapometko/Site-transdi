60afc4d6f808dff93602a22764b426d04fcb88d1
AlexandraP <apometko@ensc.fr>
2018-05-28 14:58:24 +0200

    Ajout Gain d'expérience

---
+++ levelup.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<?php include('includes/head.html'); ?>
+
+<body>
+	<?php include('includes/top.html'); ?>
+	<div class="container text-center">
+		<h1>Gain d'expérience</h1>		
+		<p>Au cours de ces deux projets, nous avons découverts d'inombrables <strong>réalités du monde du travail</strong>, notamment lorsque nous avons été confrontés à des difficultés.</p><br>
+		<img id="gh" src="images/up.png" alt="upward arrow"></img>	<br><br>
+		<div class="project">
+			<h2><strong>Le facteur humain</strong></h2>
+			<p>Les notions d'<strong>utilisabilité</strong>, <strong>d'acceptabilité</strong> et d'<strong>ergonomie</strong> étaient au coeur de nos deux projets.</p>
+			<p>Nous avons pu mettre en pratique et constater l'utilité directe des tests utilisateurs, des critères ergonomiques et des questionnaires strandardisés.</p>
+			<p>Nous avons fait des erreurs initiales dans nos tests utilisateurs, ce qui nous a permi d'identifier les <strong>points essentiels suivants</strong> :</p>
+			<ul>
+				<li>L'importance du contexte spatial et temporel des entretiens</li>
+				<li>La synthéticité des entretiens</li>
+				<li>La cohérence des sujets abordés dans l'entretien</li>
+				<li>Une formulation des questions adaptée au profil des interrogés (échelles...)</li>
+			</ul>
+			<p>Nous avons donc réelement appliqué nos connaissances en facteur humain et sommes capables de <strong>les appliquer dans un contexte professionel</strong>.</p>
+		</div>
+		<div class="project">
+			<h2><strong>La relation client</strong></h2>
+			<p>La relation avec le client est essentielle pour la bonne avancée du projet.</p>
+			<p>Pour éviter l'effet tunel qui peut amener à des insatisfactions, il est primordial de <strong>communiquer régulièrement de manière claire</strong>.</p>
+			<p>C'est ce que nous avons fait avec des échanges hebdomadaires, et cela a établi une relation de confiance avec les plusieurs intervenants avec lesquels nous avons interragis.</p>
+			<p>Nous avons constaté qu'une <strong>communication face à face</strong> est à privilégier si possible, pour éviter de nombreux échanges de mails qui ne définissent pas aussi bien les attentes qu'un entretien de vive voix.</p>
+		</div>
+		<div class="project">
+			<h2><strong>Les facteurs externes</strong></h2>
+			<p>Malgré notre bonne volonté, il faut <strong>accepter que des facteurs externes sont en jeux.</strong></p> 
+			<p>Comme par exemple :
+				<ul>
+					<li>Les délais de livraison des logiciel et du matériel que l'on doit utiliser</li>
+					<li>Les décisions des différents acteurs qui collaborent au projet, ce qui peut modifier le planning initial.</li>
+				</ul>
+			</p>
+			<p>Il faut donc trouver des <strong>stratégies de compensation</strong> pour ne pas perdre de temps à cause des retards, en s'avançant sur des parties de rédaction annexe, ou anticiper des production futures.</p>
+		</div>
+		<div class="project">
+			<h2><strong>Réussir à s'affirmer</strong></h2>
+			<p>Les changements fréquents de protocoles étaient en partie dûs au fait que nous avons eu du mal a refuser des fonctionnalités à tester qui enthousiasmaient le client.</p>
+			<p>Il est important de réelement <strong>évaluer la pertinence de ces changements et leurs effets sur le projet</strong>, et exposer notre point de vue de manière justifiée au client.</p>
+			<p>Dans notre cas, ces additions ont alourdi le protocole de test et donc affecté les résultats par la longueur de l'entretien.</p>
+		</div>
+	</div>
+	<?php include('includes/footer.html'); ?>
+
+  </body>
+</html>
